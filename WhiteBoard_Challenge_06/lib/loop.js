b17dda8d62a526be826076497bb0d31102ff47c3
Joel Clair <clair.joel@yahoo.com>
2018-01-22 15:11:00 -0800

    added documentation

--- WhiteBoard_Challenge_06/lib/loop.js
+++ WhiteBoard_Challenge_06/lib/loop.js
@@ -1,14 +1,15 @@
 'use strict';
-
+//exports the solution constructor.
 const solution = module.exports = {};
+//adds to counter after iteration through the recursive loop.
 solution.count = 0;
+//function that takes in an interger value.
 solution.loop = (count) => {
+  //if statements determines null inputs (NaN).
   if (count < 1 || typeof count !== 'number') return null;
   if (!count) return;
+  //adds to the global counter.
   solution.count ++;
+  //removes 1 from the input value to prevent an inf loop.
   solution.loop(count - 1);
-  if(count === 1) {
-    console.log('success');
-    console.log('count fin === ', solution.count);
-  }
 };
