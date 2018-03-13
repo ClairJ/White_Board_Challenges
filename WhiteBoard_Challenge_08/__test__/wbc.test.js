5d9d819c68befe7197c35818f24f26f210319ad9
Joel Clair <clair.joel@yahoo.com>
2018-03-13 14:46:40 -0700

    redo challenge 08

--- WhiteBoard_Challenge_08/__test__/wbc.test.js
+++ WhiteBoard_Challenge_08/__test__/wbc.test.js
@@ -1,13 +1,26 @@
 'use strict';
 
 const wbc = require('../lib/wbc.js');
+const sll = require('../lib/lls.js');
 
 describe('wbc.js', () => {
 
   describe('testing inputs', () => {
     it('should return null for invalid inputs', () => {
       let testLinkedList1 = 'hello';
-      expect(wbc.solution.findIntersection(testLinkedList1)).toBeNull;
+      expect(wbc.findIntersection(testLinkedList1)).toBeNull;
+    });
+
+    it('should return for invalid inputs', () => {
+      let testLinkedList1 = new sll.LinkedList;
+      testLinkedList1.add(5);
+      testLinkedList1.add(6);
+      testLinkedList1.add(7);
+      let testLinkedList2 = new sll.LinkedList;
+      testLinkedList2.add(3);
+      testLinkedList2.add(6);
+      testLinkedList2.add(9);
+      expect(wbc.findIntersection(testLinkedList1, testLinkedList2)).toBe(6);
     });
   });
 });
